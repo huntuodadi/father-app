f0cc37096a3500b3325926df2223394813d12590
zhangzy <490733118@qq.com>
2020-08-06 19:11:37 +0800

    init father app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router'
+import { Link, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
+function ReactApp() {
+  return (<div>react app</div>)
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Link to='/vue'>vue app</Link>
+        <Link to='/react'>react app</Link>
+        <Route path='/react' component={ReactApp} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { registerApplication, start } from 'single-spa';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+async function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script');
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  })
+}
+
+// single spa 的缺点 样式不隔离 没有js沙箱 手动加载script
+registerApplication('myVueApp',
+  async () => {
+    // 推荐使用system.js
+    await loadScript('http://localhost:10000/js/chunk-vendors.js');
+    await loadScript('http://localhost:10000/js/app.js');
+    return window.singleVue;
+  },
+  location => location.pathname.startsWith('/vue'),
+);
+start();
 
 ReactDOM.render(
   <React.StrictMode>
